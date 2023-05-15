b21840ed4f9dcc15abb7579d9ba70db954dbcb86
Chayannn <chayanp188@gmail.com>
2023-05-16 01:01:16 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Header from './components/header';
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
